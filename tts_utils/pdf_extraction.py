1b8fbcbda8f133fdffbbed1827cf95070885e080
YehorBoiar <boyaregor1@gmail.com>
2024-07-17 13:12:14 +0100

    Added pdf_first_page_to_jpeg function

--- tts_utils/pdf_extraction.py
+++ tts_utils/pdf_extraction.py
@@ -1,6 +1,9 @@
 from PyPDF2 import PdfReader
 from typing import Dict, Any
 from fastapi import HTTPException
+from pdf2image import convert_from_path
+from PIL import Image
+import io
 
 def pdf_to_text(file, page_num=0):
     try:
@@ -16,7 +19,6 @@ def pdf_to_text(file, page_num=0):
         raise HTTPException(status_code=400, detail="Invalid page number")
     return text
 
-
 def extract_metadata(file: str) -> Dict[str, Any]:
     reader = PdfReader(file)
     metadata = reader.metadata
@@ -25,3 +27,14 @@ def extract_metadata(file: str) -> Dict[str, Any]:
 
 def get_pages(file):
     return PdfReader(file).pages
+
+def pdf_first_page_to_jpeg(file, dpi=300):
+    images = convert_from_path(file, first_page=1, last_page=1, dpi=dpi)
+    if images:
+        img = images[0]
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format='JPEG')
+        img_byte_arr.seek(0)
+        return img_byte_arr
+    else:
+        raise HTTPException(status_code=500, detail="Failed to convert PDF to image")
