86fe343a7224a64e4b708cf122ef66c2a1a60ef8
YehorBoiar <boyaregor1@gmail.com>
2024-08-01 11:41:00 +0100

    Deleted old entity

--- db/models.py
+++ db/models.py
@@ -15,20 +15,6 @@ class User(Base):
 
     generated_wavs = relationship("GeneratedWav", order_by="GeneratedWav.wav_id", back_populates="user")
 
-
-class GeneratedWav(Base):
-    __tablename__ = 'generated_wavs'
-    
-    wav_id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(255), ForeignKey('users.username'), nullable=False)
-    pdf_file_name = Column(String(255), nullable=False)
-    wav_file_name = Column(String(255), nullable=False)
-    wav_file_path = Column(String(255), nullable=False)
-    generation_date = Column(TIMESTAMP, server_default=func.now(), nullable=False)
-
-    user = relationship("User", back_populates="generated_wavs")
-
-
 class Book(Base):
     __tablename__ = 'book'
     
